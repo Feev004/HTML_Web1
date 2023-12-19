03f5ee955103f398b09e8d57ed70dc180f3e5249
Feev004 <feevfeev2@gmail.com>
2023-12-19 20:55:54 +0700

    up

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/profile.css">
+</head>
+<body>
+    <?php
+    echo '<h1>Profile</h1>';
+    echo '<hr width="100%">';
+    echo '<center>';
+    echo '<div><img src="https://scontent.fbkk21-1.fna.fbcdn.net/v/t39.30808-6/387060453_350452920735953_2709676728739347390_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeHWEGvfEFZjXqkF57l1XYPEOtIlk4tC8Tc60iWTi0LxN1Fynmq1TEHxs0zrTerJ-sgbRaNrCMbaT0B6scLq3YTV&_nc_ohc=RDuNe04oXh4AX-DQjb8&_nc_ht=scontent.fbkk21-1.fna&oh=00_AfAxFl91soSCiekWz6h04oRmbTS2lo7YhHj6ceHqOjF8Dg&oe=657D6538" alt=""></div>';
+    echo '<p>นายรัฐศาสตร์ รจนัย</p>';
+    echo '<p>รหัส : 6506021612013</p>';
+    echo '<p>สาขา : IT</p>';
+    echo '<p>คณะเทคโนโลยีและการจัดการอุตสาหกรรม</p>';
+    echo '<p>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ วิทยาเขตปราจีนบุรี</p>';
+    echo 'ช่องทางการติดต่อ : <a href="https://www.instagram.com/fewrojjanai/">https://www.instagram.com/fewrojjanai/</a>';
+    echo '</center>';
+    echo '<div><a href="tip.php">go to tip</a></div>';
+    ?>
+</body>
+</html>
