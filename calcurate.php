d6444a281535b8307da11f51abfb506a3ef9031a
Feev004 <feevfeev2@gmail.com>
2023-12-22 00:12:08 +0700

    up

--- calcurate.php
+++ calcurate.php
@@ -6,13 +6,8 @@
     <title>calcurate</title>
     <link rel="stylesheet" href="calcurate.css">
 </head>
-<body>   
-    <?php
-    $width = 5;
-    $height = 10;
-    $sum = $width * $height;
-    ?>   
-        <table border="1" align="center" width="400">
+<body>    
+    <table border="1" align="center" width="400">
         <tr>
         <td colspan="2" align="center">
         <big>calculate</big>
@@ -20,16 +15,16 @@
         <tr>
         <tr>
         <td align="right">1</td>
-        <td align="laft">พื้นที่สี่เหลี่ยม</td>
+        <td align="laft"><a href="พื้นที่สี่เหลี่ยม.php">พื้นที่สี่เหลี่ยม</a></td>
         </tr>
         <td align="right">2</td>
-        <td align="laft">พื้นที่วงกลม</td>
+        <td align="laft"><a href="พื้นที่วงกลม.php">พื้นที่วงกลม</a></td>
         </tr>
         <td align="right">3</td>
-        <td align="laft">พื้นที่สามเหลี่ยม</td>
+        <td align="laft"><a href="พื้นที่สามเหลี่ยม.php">พื้นที่สามเหลี่ยม</a></td>
         </tr>
         <td align="right">4</td>
-        <td align="laft">พื้นที่สี่เหลี่ยมคางหมู</td>
+        <td align="laft"><a href="พื้นที่สี่เหลี่ยมคางหมู.php">พื้นที่สี่เหลี่ยมคางหมู</a></td>
         <tr>
         <td colspan="2" align="center">
         <div><a href="./index.php">profile</a></div>
