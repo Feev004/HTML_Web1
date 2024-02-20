5393a02da8b8de674b674ada779ecd02dd034b3a
Feev004 <feevfeev2@gmail.com>
2024-02-21 02:15:42 +0700

    up

--- bookList1.php
+++ bookList1.php
@@ -40,12 +40,12 @@ echo '<tr align="center" bgcolor="">';
 echo '<td>'.$row.'</td>';
 echo '<td><a
 
-href="bookDetail1_edit.php?bookId='.$rs[0].'">'.$rs[0].'</a></td>';
+href="bookDetail1_edit.php?bookid='.$rs[0].'">'.$rs[0].'</a></td>';
 echo '<td align="left">'.$rs[1].'</td>';
-echo '<td><a href="bookUpdate1.php?bookId='.$rs[0].'">[แก้ไข]
+echo '<td><a href="bookUpdate1.php?bookid='.$rs[0].'">[แก้ไข]
 
 </a></td>';
-echo '<td><a href="bookDelete1.php?bookId='.$rs[0].'"
+echo '<td><a href="bookDelete1.php?bookid='.$rs[0].'"
 onclick="return confirm(\' ยืนยันการลบข้อมูลหนังสือ '.$rs[1].'\')">[ลบ]</a></td>';
 
 echo '</tr>';
