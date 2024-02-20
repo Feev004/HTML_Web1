556d30ccc43afca4f6201dbef19d5c1470986dfb
Feev004 <feevfeev2@gmail.com>
2024-02-21 02:09:12 +0700

    up

--- bookList1.php
+++ bookList1.php
@@ -5,7 +5,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$dbName = "bookStore";
+$dbName = "bookstore";
 $conn = mysqli_connect($hostname, $username, $password);
 if (!$conn)
 die("ไม่สามารถติดต่อกับ MySQL ได้");
@@ -16,7 +16,7 @@ mysqli_select_db($conn, $dbName) or die("ไม่สามารถเลือ
 mysqli_query($conn,"set character_set_connection=utf8mb4");
 mysqli_query($conn,"set character_set_client=utf8mb4");
 mysqli_query($conn,"set character_set_results=utf8mb4");
-$sql = "select * from book order by bookId";
+$sql = "select * from book order by bookid";
 $result = mysqli_query ($conn, $sql);
 echo '<center>';
 echo '<br><h3>รายชื่อหนังสือ</h3>';
